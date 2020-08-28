5ff117a08bcede97ec8776e3ee68e28e32885e00
jesusnboone <jesusnboone@gmail.com>
2020-08-28 15:27:49 -0500

    working on comment model and routes with user rating

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const userRoutes = require('./user-routes');
-
+const commentRoutes = require('./comment-routes');
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/users', userRoutes);
-
+router.use('/comments', commentRoutes);
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -3,27 +3,27 @@ const User = require('./User');
 const Category = require('./Category');
 const Product = require('./Product');
 const Rating = require('./Rating');
-
+const Comment = require('./Comment');
 // Create associations
 
 Product.belongsTo(Category, {
     foreignKey: 'category_id',
-  });
+});
   
   
-  Category.hasMany(Product, {
+Category.hasMany(Product, {
     foreignKey: 'category_id',
-  });
+});
 
 
-  User.hasMany(Product, {
+User.hasMany(Product, {
     foreignKey: 'user_id'
-  });
+});
 
-  Product.belongsTo(User, {
+Product.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: 'SET NULL'
-  });
+});
 
 Rating.belongsTo(User, {
   foreignKey: 'rated_by'
@@ -37,6 +37,19 @@ User.hasMany(Rating, {
   foreignKey: 'user_id'
 });
 
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
 
+Comment.belongsTo(Rating, {
+  foreignKey: 'rating_id',
+  onDelete: 'SET NULL'
+});
 
-module.exports = { User, Category, Product, Rating };
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+module.exports = { User, Category, Product, Rating, Comment };
