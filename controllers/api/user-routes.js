de5671e80968c3d2dc0618ce458353a3e8843cf4
e-leal <leal.elyssa03@gmail.com>
2020-08-27 22:58:42 -0500

    Added api call for user to rate another user. /api/users/user_id is the url used

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -99,14 +99,28 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.put('/rating', (req, res) => {
+router.put('/rating/:id', (req, res) => {
+  console.log("About to rate a user!\n");
+
   if(req.session){
-    console.log("About to rate a user!\n");
-    User.rate(
-      { ...req.body, user_id: req.session.user_id },
-      {Rating, User}
+    console.log("About to rate a user!....again\n");
+    console.log("our rater is: ",req.session.user_id);
+    console.log("our user being rated is: ", req.params.id);
+    console.log("our selected rating value is: ", req.body.rating_value);
+    Rating.create(
+       {
+          rated_by: req.session.user_id,
+          user_id: req.params.id,
+          rating_value: req.body.rating_value
+        }
     )
-    .then(updatedRatingData => res.json(updatedRatingData))
+    .then(updatedRatingData => {
+      if (!updatedRatingData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(updatedRatingData);
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err); 
