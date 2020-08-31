7a6d4b4feba94c926aa140b342f4be28140a9cfe
Ctyrone123 <Ctyrone123@gmail.com>
2020-08-31 13:07:58 -0500

    Display ratings

--- controllers/render-routes.js
+++ controllers/render-routes.js
@@ -11,7 +11,7 @@ router.get("/", (req, res) => {
 
 router.get("/images", (req, res) => {
     db.Product.findAll({}).then(image => {
-        res.render("images", {images: image})
+        res.render("images", { images: image })
     })
 })
 
@@ -31,10 +31,10 @@ router.get("/products", (req, res) => {
             'user_id',
             [sequelize.literal('(SELECT username FROM user WHERE product.user_id = user.id)'), 'user']
         ]
-          
-        
+
+
     }).then(products => {
-        res.render("products", {products})
+        res.render("products", { products })
     })
 })
 
@@ -42,7 +42,7 @@ router.get("/product/:id", (req, res) => {
     db.Product.findOne({
         where: {
             id: req.params.id
-          },
+        },
         attributes: [
             'product_name',
             'description',
@@ -52,18 +52,18 @@ router.get("/product/:id", (req, res) => {
             'image',
             [sequelize.literal('(SELECT username FROM user WHERE product.user_id = user.id)'), 'user']
         ]
-          
-        
+
+
     }).then(product => {
-        res.render("single-product", {product})
+        res.render("single-product", { product })
     })
 })
 
-router.get("/seller/:id", (req, res)=>{
+router.get("/seller/:id", (req, res) => {
     db.Rating.findOne({
         where: {
             id: req.params.id
-          },
+        },
         attributes: [
             'rated_by',
             'rating_value',
@@ -72,50 +72,56 @@ router.get("/seller/:id", (req, res)=>{
             //[sequelize.literal('(SELECT AVG(rating_value) FROM rating WHERE rating.rated_by = user.id)'), 'rating_avg']
         ]
     })
-    .then(ratings => {
-        res.render("seller", {ratings})
-    })
+        .then(ratings => {
+            res.render("rating", { ratings })
+        })
 })
 
 router.get('/rating/:id', (req, res) => {
     console.log("About to rate a user!\n");
-  
-    if(req.session){
-      console.log("About to rate a user!....again\n");
-      console.log("our rater is: ",req.session.user_id);
-      console.log("our user being rated is: ", req.params.id);
-      console.log("our selected rating value is: ", req.body.rating_value);
-      db.Rating.findAll(
-         {
-            rated_by: req.session.user_id,
-            user_id: req.params.id,
-            rating_value: req.body.rating_value
-          }
-      )
-      .then(updatedRatingData => {
-        if (!updatedRatingData) {
-          res.status(404).json({ message: 'No post found with this id' });
-          return;
-        }
-        res.json(updatedRatingData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err); 
-      });
-    }
-  });
 
-router.get("/homepage", (req, res) =>{
+    if (req.session) {
+        console.log("About to rate a user!....again\n");
+        console.log("our rater is: ", req.session.user_id);
+        console.log("our user being rated is: ", req.params.id);
+        console.log("our selected rating value is: ", req.body.rating_value);
+        db.Rating.findAll(
+            {
+                where: {
+                    user_id: req.params.id
+                }
+            }
+            ,
+            {
+                rated_by: req.session.user_id,
+                user_id: req.params.id,
+                rating_value: req.body.rating_value
+            }
+        )
+            .then(updatedRatingData => {
+                if (!updatedRatingData) {
+                    res.status(404).json({ message: 'No post found with this id' });
+                    return;
+                }
+                res.render("rating", { updatedRatingData });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
+});
+
+router.get("/homepage", (req, res) => {
     res.render("index2");
 })
 router.get('/login', (req, res) => {
- //   if (req.session.loggedIn) {
-  //  res.redirect('/');
+    //   if (req.session.loggedIn) {
+    //  res.redirect('/');
     //return;
-   // }
-  
+    // }
+
     res.render('login');
-  });
+});
 
 module.exports = router
