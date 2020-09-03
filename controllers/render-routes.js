3ea327055c747ad896b0e965d94b9fec4f99c5d8
jesusnboone <jesusnboone@gmail.com>
2020-09-02 22:43:56 -0500

    test save

--- controllers/render-routes.js
+++ controllers/render-routes.js
@@ -83,7 +83,7 @@ router.get('/user/:id', (req, res) => {
         // we're establishing this route to render products.handlebars
         // we're also passing through the sequelize data that our route gives us
         // this data is established as products, for handlebars to use in the products.handlebars page
-        res.render("products", { products, loggedIn: req.session.loggedIn })
+        res.render("user", { products, loggedIn: req.session.loggedIn })
     })
 })
 
